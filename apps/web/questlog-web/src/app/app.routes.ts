8d7e791e61b42649ed083fda22aef4f6a2055661
Peter Steudel <peter.steudel@outlook.com>
2026-03-04 21:51:26 +0100

    routing tavern & campaign map (without page content)

--- apps/web/questlog-web/src/app/app.routes.ts
+++ apps/web/questlog-web/src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { HomePageComponent } from './presentation/views/home-page.component/home-page.component';
+import {StructurePageComponent } from './presentation/views/structure-page.component/structure-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'home', component: HomePageComponent},
+    { path: 'structure', component: StructurePageComponent},
+    {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'
+    },
+    {
+        path: '**',
+        redirectTo: 'home',
+        pathMatch: 'full'
+    }
+];
---
+++ apps/web/questlog-web/src/app/presentation/views/structure-page.component/structure-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-structure-page.component',
+  imports: [],
+  templateUrl: './structure-page.component.html',
+  styleUrl: './structure-page.component.scss',
+})
+export class StructurePageComponent {
+
+}
