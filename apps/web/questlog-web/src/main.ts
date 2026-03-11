f72ea422d9a76533859a7147301ffc43664d5600
Peter Steudel <peter.steudel@outlook.com>
2026-03-11 21:13:54 +0100

    Refactor application structure by moving app configuration and main component files to appropriate directories

--- apps/web/questlog-web/src/main.ts
+++ apps/web/questlog-web/src/main.ts
@@ -1,6 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/presentation/app.config';
-import { App } from './app/app';
+import { appConfig } from './app/app.config';
+import { App } from './app/presentation/app';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 bootstrapApplication(App, appConfig)
